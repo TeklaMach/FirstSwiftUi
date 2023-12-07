506869797c1fed3f12d19419feb0d250fb838350
TeklaMach <macharashvilitekla@gmail.com>
2023-12-07 08:17:48 +0400

    Initial Commit

---
+++ FirstSwiftUI/FirstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIApp.swift
+//  FirstSwiftUI
+//
+//  Created by Tekla Matcharashvili on 07.12.23.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
